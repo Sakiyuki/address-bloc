2c44f375f389b14ef7573900ac42bcb71e92e389
Bryant Caldwell <cwellionaire@gmail.com>
2016-05-09 18:34:54 -0400

    completed checkpoint 21-CSV's

--- models/address_book.rb
+++ models/address_book.rb
@@ -1,5 +1,5 @@
 require_relative 'entry.rb'
-
+require "csv"
 class AddressBook
   attr_reader :entries
 
@@ -12,16 +12,27 @@ class AddressBook
   end
 
   def add_entry(name, phone_number, email)
-   # #9
-   index = 0
-   entries.each do |entry|
-    # #10
-    if name < entry.name
-      break
+    # #9
+    index = 0
+    entries.each do |entry|
+      # #10
+      if name < entry.name
+        break
+      end
+      index += 1
     end
-    index += 1
+    # #11
+    entries.insert(index, Entry.new(name, phone_number, email))
   end
-  # #11
-  entries.insert(index, Entry.new(name, phone_number, email))
- end
+
+  def import_from_csv(file_name)
+    csv_text = File.read(file_name)
+    csv = CSV.parse(csv_text, headers: true, skip_blanks: true)
+
+    csv.each do |row|
+      row_hash = row.to_hash
+      add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
+    end
+  end
+
 end
